791131d8b25272bee6cbb40455eee15893053d51
sreeDale <sreekanthiuk@gmail.com>
2021-09-11 08:36:42 +0100

    add new a method and identition and commented unused code

--- Datafile.py
+++ Datafile.py
@@ -1,86 +1,67 @@
 import csv
-from itertools import islice
 
 
 class SampleDataProcessing:
+    import csv
 
-
-    def calc_pos_column( header , column_name):
-        curr_posn=0
+    def calc_posn_of_column(header, column_name):
+        curr_posn = 0
         for i in header:
-            if i==column_name:
-               return curr_posn
+            if i == column_name:
+                return curr_posn
             else:
-               curr_posn =curr_posn + 1
+                curr_posn += 1
 
-    def read_csv_rawdata(self,myfile):
-        dict_of_offense_desc = {}
-        with open(myfile) as csv_file:
-            cvs_reader=csv.reader(csv_file)
-            line_count=0
-            for row in cvs_reader:
-                if line_count ==0:
+    def read_csv(self, my_file, grouping_column):
+        dict_of_offence_desc = {}
+        with open(my_file) as csv_file:
+            csv_reader = csv.reader(csv_file)
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
                     print(row)
-                    column_posn=SampleDataProcessing.calc_pos_column(row,'OFNS_DESC')
-                    print('position of the column list'+ str(column_posn))
+                    column_posn = SampleDataProcessing.calc_posn_of_column(row, grouping_column)
+                    print('position of column is:' + str(column_posn))
                 else:
-                    if row[column_posn] not in dict_of_offense_desc:
-                        dict_of_offense_desc[row[column_posn]] = 1
+                    if row[column_posn] not in dict_of_offence_desc:
+                        dict_of_offence_desc[row[column_posn]] = 1
                     else:
-                        dict_of_offense_desc[row[column_posn]]+=1
+                        dict_of_offence_desc[row[column_posn]] += 1
 
-                line_count+=1
-
-        sorted_dict_of_offence_desc = {k: v for k, v in sorted(dict_of_offense_desc.items(), key=lambda item: item[1],
+                line_count += 1
+        sorted_dict_of_offence_desc = {k: v for k, v in sorted(dict_of_offence_desc.items(), key=lambda item: item[1],
                                                                reverse=True)}
-        
+
         return sorted_dict_of_offence_desc
 
+    def write_to_csv(self, input_file, output_file, lookup_column, keyword):
+        with open(input_file) as csv_file, open(output_file, 'w', newline="") as file:
+            csv_reader = csv.reader(csv_file)
+            writer = csv.writer(file)
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    print(row)
+                    column_posn = SampleDataProcessing.calc_posn_of_column(row, lookup_column)
+                    writer.writerow(row)
+                else:
+                    if keyword in row[column_posn]:
+                        writer.writerow(row)
+                line_count += 1
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     read_and_count = SampleDataProcessing()
 
-    read_file = read_and_count.read_csv_rawdata('C:/NYPD/nypd-arrest-data-2018-1.csv')
+    read_file = read_and_count.read_csv('C:/NYPD/nypd-arrest-data-2018-1.csv'
+                                  , 'OFNS_DESC')
+
     dict_items = read_file.items()
-    print(" Requiremt :Produce a dictionary count records group by OFNS_DESC in descending order ")
-    print("-----------------------------------------------------------------------")
-    first_10= list(dict_items)[:10]
-    iterator=islice(first_10,10)
-    for item in iterator:
-     print(item)
-    print("---------------------------------------------------------------------------") 
-    print("list of Offense descriptions")
-    for str in dict_items:
-        print(str)
-#offense_desc=input("Enter Offense description")
-#print(offense_desc)
- 
-    
- 
 
+    first_10 = list(dict_items)[:10]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print(first_10)
+    uvalue=input("Enter your value :") 
+    print(uvalue)
+    read_and_count.write_to_csv('C:/NYPD/nypd-arrest-data-2018-1.csv',
+                                'C:/NYPD/output.csv', 'OFNS_DESC', uvalue)
